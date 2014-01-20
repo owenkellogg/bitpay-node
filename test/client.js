b344baf4cc2a5d4ec4f26db703a1570ece83ecd6
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-20 12:37:20 -0800

    [TEST] Client creates a real Bitpay invoice with valid creds.

--- test/client.js
+++ test/client.js
@@ -4,15 +4,35 @@ var assert = require('assert');
 describe('Bitpay.Client', function() {
 
   before(function() {
-    client = new Bitpay.Client({ apiKey: '1234567' });
   });
 
   it('should fail to create an invoice with bad creds', function(done) {
-    client.createInvoice(function(err, invoice){
+    client = new Bitpay.Client({ apiKey: '1234567' });
+    client.createInvoice({}, function(err, invoice){
       assert.equal(err.type, 'unauthorized');
       assert.equal(err.message, 'invalid api key');
       done();
     }); 
+  });
+
+  it('it should create a bitpay invoice', function(done) {
+    var apiKey = process.env.BITPAY_API_KEY;
+    if (apiKey) {
+      client = new Bitpay.Client({ apiKey: apiKey });   
+      client.createInvoice({ price: 0.001, currency: 'BTC' }, function(err, invoice) {
+        assert(!!!err);
+        assert.equal(invoice.price, 0.001);
+        assert.equal(invoice.currency, 'BTC');
+        assert.equal(invoice.btcPrice, '0.0010');
+        assert.equal(invoice.status, 'new');
+        done();
+      });
+
+    } else { 
+      console.log('CONFIG: set ENV variable BITPAY_API_KEY and re-run test');
+      assert(false); 
+      done(); 
+    }
   });
 
 });
